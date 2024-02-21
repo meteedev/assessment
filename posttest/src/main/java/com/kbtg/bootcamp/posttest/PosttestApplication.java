fb2b4f9cd7d9bf13ed04a8068ea6ff610c6ce899
Metee Srisawatvichai <metee.dev@gmail.com>
2024-02-21 18:31:23 +0700

    remove scan package annotation

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.kbtg.bootcamp.posttest.lottery.model.entity")
 public class PosttestApplication {
 
 	public static void main(String[] args) {
